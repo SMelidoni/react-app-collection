0acea8389785959d7156d78cc722676125e90702
SMelidoni <simone.melidoni@gmail.com>
2023-04-03 23:09:03 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,13 @@
-import React, { FC, lazy, Suspense } from 'react';
+import React, { FC } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import MainMenu from './pages/main-menu/main-menu.component';
-
-import ReactCalculatorApp from '../react-calculator-app/src/App';
 
 const App: FC = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<MainMenu />} />
-        <Route
-          path="/react-calculator-app"
-          element={
-            <Suspense fallback={<div>Loading...</div>}>
-              <ReactCalculatorApp />
-            </Suspense>
-          }
-        />
       </Routes>
     </Router>
   );
