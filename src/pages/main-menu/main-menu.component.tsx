df0bce8d335b151eeadeb892eb4735227f134734
SMelidoni <simone.melidoni@gmail.com>
2023-04-05 22:07:12 +0100

    Added initial main menu screen w/ buttons

--- src/pages/main-menu/main-menu.component.tsx
+++ src/pages/main-menu/main-menu.component.tsx
@@ -2,10 +2,18 @@ import '../main-menu/main-menu.styles.scss';
 
 import React, { FC } from 'react';
 
+import GlobalButton from '../../components/global-button/global-button.component';
+
 const MainMenu: FC = () => {
   return (
     <div className="main-menu-container">
       <header className="main-menu-header">Main Menu</header>
+      <GlobalButton label="Home" altText="Go to the home page" route="/" />
+      <GlobalButton
+        label="Calculator"
+        altText="Go to the react calculator app"
+        route="/calculator"
+      />
     </div>
   );
 };
