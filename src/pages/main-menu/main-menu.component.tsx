cba87967712a707303c43ce21568c36a3ef83068
Simone Melidoni <simone.melidoni@gmail.com>
2023-04-06 10:29:28 +0100

    feature/retro-main-menu (#8)
    * Updated main menu to have a retro style
    
    * Updated styling and changelog
    
    * Update CHANGELOG.md
    
    ---------
    
    Co-authored-by: Simone Melidoni <112873106+simone-melidoni@users.noreply.github.com>

--- src/pages/main-menu/main-menu.component.tsx
+++ src/pages/main-menu/main-menu.component.tsx
@@ -6,14 +6,16 @@ import GlobalButton from '../../components/global-button/global-button.component
 
 const MainMenu: FC = () => {
   return (
-    <div className="main-menu-container">
-      <header className="main-menu-header">Main Menu</header>
-      <GlobalButton label="Home" altText="Go to the home page" route="/" />
-      <GlobalButton
-        label="Calculator"
-        altText="Go to the react calculator app"
-        route="/calculator"
-      />
+    <div className="global-page-container">
+      <div className="main-menu-container">
+        <header className="main-menu-header">Main Menu</header>
+        <GlobalButton label="Home" altText="Go to the home page" route="/" />
+        <GlobalButton
+          label="Calculator"
+          altText="Go to the react calculator app"
+          route="/calculator"
+        />
+      </div>
     </div>
   );
 };
