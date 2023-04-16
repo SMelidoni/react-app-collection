318985dec82569e9629163fa2214f43660813989
Simone Melidoni <simone.melidoni@gmail.com>
2023-04-16 19:43:01 +0100

    feature/pixelated-grid (#17)
    * Added pixelated grid background style
    
    * Removed global button background colour

--- src/pages/main-menu/main-menu.component.tsx
+++ src/pages/main-menu/main-menu.component.tsx
@@ -7,6 +7,7 @@ import GlobalButton from '../../components/global-button/global-button.component
 const MainMenu: FC = () => {
   return (
     <div className='global-page-container'>
+      <div className='pixelated-grid'></div>
       <div className='main-menu-container'>
         <header className='main-menu-header'>React App Collection</header>
         <h1 className='main-menu-sub-header'>Main Menu</h1>
