26311c01a1ea9d32376d70bfb62c0b8a1078e5c6
SMelidoni <simone.melidoni@gmail.com>
2023-06-19 14:03:55 +0100

    Removed unecessary code

--- src/pages/todo-home/todo-home.component.tsx
+++ src/pages/todo-home/todo-home.component.tsx
@@ -7,19 +7,16 @@ import Todo from '../../components/todo/todo.component';
 
 const ToDoHome: FC = () => {
   return (
-    <div className='global-page-container'>
-      <div className='pixelated-grid'></div>
-      <div className='todo-home-container'>
-        <div className='todo-home-back-button'>
-          <GlobalButton label='<- Back' route='/' />
-        </div>
-        <header className='todo-home-header'>
-          <h1 className='header-text header-text-react'>React</h1>
-          <h1 className='header-text header-text-todo'>To Do</h1>
-        </header>
-        <div className='todo-home-content'>
-          <Todo />
-        </div>
+    <div className='todo-home-container'>
+      <div className='todo-home-back-button'>
+        <GlobalButton label='<- Back' route='/' />
+      </div>
+      <header className='todo-home-header'>
+        <h1 className='header-text header-text-react'>React</h1>
+        <h1 className='header-text header-text-todo'>To Do</h1>
+      </header>
+      <div className='todo-home-content'>
+        <Todo />
       </div>
     </div>
   );
